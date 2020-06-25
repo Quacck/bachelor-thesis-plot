2997042b421998b2581bbf87159939d564defa36
Vincent Opitz <vincent.opitz@student.hpi.de>
2020-06-25 12:19:39 +0200

    only generate relevant plots

--- plot_accuracy_over_weights.py
+++ plot_accuracy_over_weights.py
@@ -61,82 +61,105 @@ for size in range(len(runSizesToIndex)):
                 values = np.float_(values)
                 ratioOfWronglyCorrelatedAveraged[size][time][spacial][direction] = mean(values)
 
-# print(correlatedRunCountsAveraged)
-
-# print(ratioOfWronglyCorrelatedAveraged)
-
-# create a matrix that show the count of train runs accross timeweights and distanceweights
-for key in runSizesToIndex:
-    sizeIndex = runSizesToIndex[key]
-
-    heatmap = np.zeros([5,5])
-    heaptMapDistanceWeight = 1
-    for time in range(len(timeweights)):
-        for spacial in range(len(spacialweights)):
-            heatmap[len(timeweights) - time - 1][spacial] = correlatedRunCountsAveraged[sizeIndex][time][spacial][heaptMapDistanceWeight] / key
-            # heatmap[len(timeweights) - time - 1][spacial] = time/4
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(heatmap)
-
-    ax.set_xticks(np.arange(len(timeweights)))
-    ax.set_yticks(np.arange(len(spacialweights)))
-
-    ax.set_title('ratio of outcoming trainruns with ' + str(key) + ' trains as input')
-
-    ax.set_xlabel('weight of distance')
-    ax.set_ylabel('weight of time')
+fig, (ax2, ax1) = plt.subplots(1, 2)
+fig.tight_layout()
+# fig.suptitle('Korrelationsgenauigkeit bei zwei verschiedenen Zuganzahlen')
+plt.subplots_adjust(wspace=0.3)
 
 
-    ax.set_xticklabels(timeweights)
-    ax.set_yticklabels(spacialweights)
+#first diagram
+heatmap = np.zeros([5,5])
+heaptMapDistanceWeight = 1
+for time in range(len(timeweights)):
+    for spacial in range(len(spacialweights)):
+        # the (0,0) is in the bottem left corner, so we have to switch around the y axis,
+        # also convert to correct correlation by using 1 - value
+        heatmap[len(timeweights) - time - 1][spacial] = 1 - ratioOfWronglyCorrelatedAveraged[runSizesToIndex[50]][time][spacial][heaptMapDistanceWeight]
+        # heatmap[len(timeweights) - time - 1][spacial] = time/4
+im1 = ax1.imshow(heatmap, cmap='brg', vmin=0.3, vmax=0.7)
 
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-            rotation_mode="anchor")
+ax1.set_xticks(np.arange(len(timeweights)))
+ax1.set_yticks(np.arange(len(spacialweights)))
 
-    for i in range(len(timeweights)):
-        for j in range(len(spacialweights)):
-            text = ax.text(j, i, round(heatmap[i, j],3),
-                        ha="center", va="center", color="w")
+ax1.set_title('50 Züge')
 
-
-# show correlation accuracy
-for key in runSizesToIndex:
-    sizeIndex = runSizesToIndex[key]
-
-    heatmap = np.zeros([5,5])
-    heaptMapDistanceWeight = 1
-    for time in range(len(timeweights)):
-        for spacial in range(len(spacialweights)):
-            # the (0,0) is in the bottem left corner, so we have to switch around the y axis,
-            # also convert to correct correlation by using 1 - value
-            heatmap[len(timeweights) - time - 1][spacial] = 1 - ratioOfWronglyCorrelatedAveraged[sizeIndex][time][spacial][heaptMapDistanceWeight]
-            # heatmap[len(timeweights) - time - 1][spacial] = time/4
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(heatmap)
-
-    ax.set_xticks(np.arange(len(timeweights)))
-    ax.set_yticks(np.arange(len(spacialweights)))
-
-    ax.set_title('correlation accuracy ' + str(key) + ' trains as input')
-
-    ax.set_xlabel('weight of distance')
-    ax.set_ylabel('weight of time')
+ax1.set_xlabel('Zeitgewichtung')
+ax1.set_ylabel('Ortsgewichtung')
 
 
-    ax.set_xticklabels(timeweights)
-    ax.set_yticklabels(spacialweights)
+ax1.set_xticklabels(timeweights)
+ax1.set_yticklabels(spacialweights)
 
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-            rotation_mode="anchor")
+plt.setp(ax1.get_xticklabels(), rotation=45, ha="right",
+        rotation_mode="anchor")
 
-    for i in range(len(timeweights)):
-        for j in range(len(spacialweights)):
-            text = ax.text(j, i, round(heatmap[i, j],3),
-                        ha="center", va="center", color="w")
+for i in range(len(timeweights)):
+    for j in range(len(spacialweights)):
+        text = ax1.text(j, i, round(heatmap[i, j],3),
+                    ha="center", va="center", color="black")
 
-# generate a graph for time and distance being weighted by 1 with different 
+#second diagram
+heatmap = np.zeros([5,5])
+heaptMapDistanceWeight = 1
+for time in range(len(timeweights)):
+    for spacial in range(len(spacialweights)):
+        # the (0,0) is in the bottem left corner, so we have to switch around the y axis,
+        # also convert to correct correlation by using 1 - value
+        heatmap[len(timeweights) - time - 1][spacial] = 1 - ratioOfWronglyCorrelatedAveraged[runSizesToIndex[10]][time][spacial][heaptMapDistanceWeight]
+        # heatmap[len(timeweights) - time - 1][spacial] = time/4
+im1 = ax2.imshow(heatmap, cmap='brg', vmin=0.3, vmax=0.7)
+
+ax2.set_xticks(np.arange(len(timeweights)))
+ax2.set_yticks(np.arange(len(spacialweights)))
+
+ax2.set_title('10 Züge')
+
+ax2.set_xlabel('Zeitgewichtung')
+ax2.set_ylabel('Ortsgewichtung')
+
+
+ax2.set_xticklabels(timeweights)
+ax2.set_yticklabels(spacialweights)
+
+plt.setp(ax2.get_xticklabels(), rotation=45, ha="right",
+        rotation_mode="anchor")
+
+for i in range(len(timeweights)):
+    for j in range(len(spacialweights)):
+        text = ax2.text(j, i, round(heatmap[i, j],3),
+                    ha="center", va="center", color="black")
+
+# generate a graph for time and distance being static with different direction weights 
+timeWeight = 0.5
+distanceWeight = 0.75
+runSize = 50
+
+accuracies = []
+
+for directionalWeight in directionalweights:
+    accuracies.append(ratioOfWronglyCorrelatedAveraged[runSizesToIndex[50]][int(timeWeight * 4)][int(distanceWeight * 4)][int(directionalWeight*4)])
+
+print(accuracies)
+print(directionalweights)
+
+# plt.plot(accuracies, [0, 0.25, 0.5, 0.75, 1])
+
+fig, directional = plt.subplots()
+# fig.suptitle('Korrelationsgenauigkeit bei unterschiedlicher Richtungsgewichtung bei 50 Zügen \n Zeitgewichtung = 0.5, Ortsgewichtung = 0.75')
+
+
+im = directional.plot(directionalweights, accuracies)
+directional.set_ylim([0.4, 0.6])
+# directional.set_xticks(np.arange(len(directionalweights)))
+# directional.set_xticklabels(directionalweights)
+
+for j in range(len(directionalweights)):
+        text = directional.text(0, j, directionalweights[j],
+                    ha="center", va="center", color="black")
+
+
+directional.set_xlabel('Richtungsgewichtung')
+directional.set_ylabel('Korrelationsgenauigkeit')
 
 plt.legend()
 plt.show()
